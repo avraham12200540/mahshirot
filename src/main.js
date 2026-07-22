6e076acf09ed69bc86b6a938e070d9e22e60e448
shlomoraviv <0556798858b@gmail.com>
2026-07-23 00:32:42 +0300

    Add Pavv section to main layout

--- src/main.js
+++ src/main.js
@@ -17,6 +17,7 @@ import "./styles/modal.css";
 import "./styles/banner.css";
 import "./styles/file-manager.css";
 import "./styles/footer.css";
+import "./styles/pavv-section.css";
 import "./styles/responsive.css";
 
 import { el, svg, $ } from "./core/dom.js";
@@ -31,6 +32,7 @@ import { createToolbar } from "./ui/toolbar.js";
 import { createMainCards } from "./ui/cards.js";
 import { createLogPanel } from "./ui/log-panel.js";
 import { createFooter } from "./ui/footer.js";
+import { createPavvSection } from "./ui/pavv-section.js";
 import { openSourcesGuide, openAboutModal, openDebuggingGuide } from "./ui/guides.js";
 
 /* ==========================================================================
@@ -120,7 +122,15 @@ function mount() {
   });
 
   const main = el("main.main", {}, [
-    el("div.container", {}, [browserBanner(), toolbar, results, cards, actionsRow(), pinnedWarning()]),
+    el("div.container", {}, [
+      browserBanner(),
+      toolbar,
+      results,
+      cards,
+      actionsRow(),
+      pinnedWarning(),
+      createPavvSection(),
+    ]),
   ]);
 
   const { panel } = createLogPanel();
